bb7fdf1ce6182671e9e38757463c7b4e293bc724
Patrice Thiré <patrice.thire@gmail.com>
2017-12-13 14:16:03 +0100

    essai avec connect corrigé

--- server.js
+++ server.js
@@ -80,4 +80,4 @@ app.use(function(req, res){
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
-http.createServer(app).listen(port);
+http.createServer(app).listen({"host": ip, "port" : port});
